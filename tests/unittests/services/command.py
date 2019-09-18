6d56227f266b81d6c17815e741c86c3202055925
Aditya Bansal <adi.bansal241996@gmail.com>
2019-09-18 15:57:56 +0530

    chore(tests): update/fix integration and unit tests post storyhub upgrade

--- tests/unittests/services/command.py
+++ tests/unittests/services/command.py
@@ -1,14 +1,50 @@
-from sls.services.command import Command
+from sls.services.command import Command as CommandCompletionItem
+
+from storyhub.sdk.service.Command import Command
 
 
-def test_args():
-    arg1 = 1
-    arg2 = 2
+def test_args(magic):
     args = {
-        'arg1': arg1,
-        'arg2': arg2,
+        'arg1': {
+            'name': 'foo',
+            'help': 'Test arg 1',
+            'type': 'int'
+        },
+        'arg2': {
+            'name': 'bar',
+            'help': 'Test arg 2',
+            'type': 'int'
+        },
     }
-    command = Command('name', 'desc', args)
-    command.arg('arg1') == 1
-    command.arg('arg2') == 2
-    assert [*command.args()] == [1, 2]
+    command_dict = {
+        'name': 'foobar',
+        'command': {
+            'help': 'Test command',
+            'arguments': args,
+        }
+    }
+    context = magic()
+    expected_completion_dict = {
+        'label': 'foobar',
+        'kind': 11,
+        'detail': 'Command: Test command',
+        'documentation': 'Test command',
+        'textEdit': {
+            'range': {
+                'start': {
+                    'line': context.pos.line,
+                    'character': context.pos.char - len(context.word)
+                },
+                'end': {
+                    'line': context.pos.line,
+                    'character': context.pos.char
+                }
+            },
+            'newText': 'foobar '
+        }
+    }
+
+    command = Command.from_dict(data=command_dict)
+    command_completion = CommandCompletionItem(command)
+    result = command_completion.to_completion(context)
+    assert result == expected_completion_dict
