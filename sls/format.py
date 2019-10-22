9c373eb84e3782417313d79d7f1ccc2e3f1d22a6
Sebastian Wilzbach <seb@wilzba.ch>
2019-10-22 14:31:06 +0200

    Implement formatting capability

--- sls/format.py
+++ sls/format.py
@@ -1,6 +1,22 @@
+from storyscript.Story import Story
+
+
 class Formatter():
     """
     Pretty-print source code.
     """
     def format(self, ws, doc):
-        return 'TBD - Formatting'
+        last_line = doc.nr_lines() - 1
+        story = Story(doc.text(), features=None)
+        new_story = story.parse(parser=None).format()
+        resp = [{
+            'range': {
+                'start': {'line': 0, 'character': 0},
+                'end': {
+                    'line': last_line,
+                    'character': len(doc.line(last_line))
+                },
+            },
+            'newText': new_story,
+        }]
+        return resp
