1fe43f37df56a6e6ef31f272049617feaa45b28f
Sebastian Wilzbach <seb@wilzba.ch>
2019-11-14 20:42:57 +0100

    Add test for JSON message errors

--- tests/integration/lsp/websocket.py
+++ tests/integration/lsp/websocket.py
@@ -22,7 +22,7 @@ def ws(patch, magic):
     return ws()
 
 
-def test_open(ws):
+def test_message(ws):
     ws.open()
     ws.on_message(json.dumps({
         'jsonrpc': '2.0',
@@ -42,6 +42,13 @@ def test_open(ws):
         '{"resolveProvider": false, "triggerCharacters": ["."]}, '
         '"hoverProvider": true, "documentFormattingProvider": true, '
         '"textDocumentSync": {"openClose": true, "change": 1}}}}')
+
+
+def test_message_error(patch, ws):
+    ws.open()
+    patch.object(ws._ls.endpoint, 'consume')
+    ws.on_message('abc')
+    ws._ls.endpoint.consume.assert_not_called()
 
 
 def test_check_origin(ws):
